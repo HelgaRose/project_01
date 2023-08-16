20b012d1e61c03d7391b3be38f3e74443de2c335
HelgaRose <137286904+HelgaRose@users.noreply.github.com>
2023-08-16 19:29:42 +0500

    add task 1_2CD

---
+++ lvl_1/complete_task_1_2 CD.py
@@ -0,0 +1,90 @@
+# Задача 1.2.
+
+# Пункт A. 
+# Приведем плейлист песен в виде списка списков
+# Список my_favorite_songs содержит список названий и длительности каждого трека
+# Выведите общее время звучания трех случайных песен в формате
+# Три песни звучат ХХХ минут
+
+my_favorite_songs = [
+    ['Waste a Moment', 3.03],
+    ['New Salvation', 4.02],
+    ['Staying\' Alive', 3.40],
+    ['Out of Touch', 3.03],
+    ['A Sorta Fairytale', 5.28],
+    ['Easy', 4.15],
+    ['Beautiful Day', 4.04],
+    ['Nowhere to Run', 2.58],
+    ['In This World', 4.02],
+]
+
+# Пункт B. 
+# Есть словарь песен 
+# Распечатайте общее время звучания трех случайных песен
+# Вывод: Три песни звучат ХХХ минут.
+
+my_favorite_songs_dict = {
+    'Waste a Moment': 3.03,
+    'New Salvation': 4.02,
+    'Staying\' Alive': 3.40,
+    'Out of Touch': 3.03,
+    'A Sorta Fairytale': 5.28,
+    'Easy': 4.15,
+    'Beautiful Day': 4.04,
+    'Nowhere to Run': 2.58,
+    'In This World': 4.02,
+}
+
+# Дополнительно для пунктов A и B
+# Пункт C.
+# Сгенерируйте случайные песни с помощью модуля random
+# import random
+
+# Дополнительно 
+# Пункт D.
+# Переведите минуты и секунды в формат времени. Используйте модуль datetime 
+
+print("C.")
+import random
+my_favorite_songs = [
+    ['Waste a Moment', 3.03],
+    ['New Salvation', 4.02],
+    ['Staying\' Alive', 3.40],
+    ['Out of Touch', 3.03],
+    ['A Sorta Fairytale', 5.28],
+    ['Easy', 4.15],
+    ['Beautiful Day', 4.04],
+    ['Nowhere to Run', 2.58],
+    ['In This World', 4.02],
+]
+
+y = len(my_favorite_songs)
+x1 = random.randint (0,y-1)
+x2 = random.randint (0,y-1)
+x3 = random.randint (0,y-1)
+total_time = my_favorite_songs[x1][1] + my_favorite_songs[x2][1] + my_favorite_songs[x3][1]
+print(f"Три песни: {my_favorite_songs[x1][0]}, {my_favorite_songs[x2][0]}, {my_favorite_songs[x3][0]} звучат: {round(total_time,2)} минут")
+
+
+print("D.")
+
+import datetime
+
+my_favorite_songs = [
+    ['Waste a Moment', 3.03],
+    ['New Salvation', 4.02],
+    ['Staying\' Alive', 3.40],
+    ['Out of Touch', 3.03],
+    ['A Sorta Fairytale', 5.28],
+    ['Easy', 4.15],
+    ['Beautiful Day', 4.04],
+    ['Nowhere to Run', 2.58],
+    ['In This World', 4.02],
+]
+
+
+y = len(my_favorite_songs)
+for i in range(0, y):
+  minutes = my_favorite_songs[i][1]
+  time_song = datetime.timedelta(minutes=minutes)
+  print(f'Длительность песни "{my_favorite_songs[i][0]}" - {time_song} ч.м.с')
